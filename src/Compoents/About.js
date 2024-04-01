4cfcf5b13eec68fd614397950e731af8869600fe
AndreTaq <andretaqian@gmail.com>
2024-03-31 18:41:22 -0700

    Created functions file and added scss function to the about page btn

--- src/Compoents/About.js
+++ src/Compoents/About.js
@@ -8,7 +8,6 @@ export default function About() {
             <h1>About Page</h1>
             <img src="https://cdn.pixabay.com/photo/2016/05/05/02/37/sunset-1373171_1280.jpg"></img>
             <button>Click Here!</button>
-            <button>Click Here!</button>
         </div>
     );
 }
